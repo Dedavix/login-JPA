ed23089073528f159adcaf751d57301ce232987e
dedav <dedav@192.168.10.57>
2020-01-21 16:20:40 +0100

    initialized jpa project

---
+++ login-Jpa/src/test/java/it/objectmethod/loginJpa/LoginJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.objectmethod.loginJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
