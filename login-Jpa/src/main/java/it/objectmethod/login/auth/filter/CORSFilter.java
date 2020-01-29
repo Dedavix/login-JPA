2a389d32899e3188b3493bdfbda40faf79b7bde8
Dedavix <dedavix@hotmail.com>
2020-01-29 18:42:29 +0100

    fix

---
+++ login-Jpa/src/main/java/it/objectmethod/login/auth/filter/CORSFilter.java
@@ -0,0 +1,44 @@
+package it.objectmethod.login.auth.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		System.out.println("CORSFilter HTTP Request: " + request.getMethod());
+
+		// Authorize (allow) all domains to consume the content
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods",
+				"GET, OPTIONS, HEAD, PUT, POST");
+		((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "token");
+
+		HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+		// For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS
+		// handshake
+		if (request.getMethod().equals("OPTIONS")) {
+			resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+			return;
+		}
+
+		// pass the request along the filter chain
+		chain.doFilter(request, servletResponse);
+
+	}
+
+}
--- login-Jpa/src/main/java/it/objectmethod/login/controller/UserRestController.java
+++ login-Jpa/src/main/java/it/objectmethod/login/controller/UserRestController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
